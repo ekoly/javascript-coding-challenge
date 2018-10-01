ec7855885fa5d8710874dbd7a7bb5eb3021fcc31
cowley <ecowley@protonmail.com>
2018-10-01 11:37:39 -0400

    added form and form-submission

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import polka from 'polka';
 import send from '@polka/send-type';
 import cors from 'cors';
 import fetch from 'node-fetch';
+import fs from 'fs';
+import uuid from 'uuid/v4';
 
 import html from './html.js';
 import users from './users.js';
@@ -16,6 +18,7 @@ const app = polka();
 app.use(cors());
 
 app.get('/healthcheck', (_, res) => res.end('OK'));
+
 app.post('/mocky-api', async (req, res) => {
   const url = 'http://www.mocky.io/v2/5b60920b2f00008e364619ee?mocky-delay=' + Math.floor(Math.random() * 3e3) + 'ms';
   
@@ -42,6 +45,7 @@ app.post('/mocky-api', async (req, res) => {
         { 'Content-Type': 'application/json' });
   }
 });
+
 app.post('/data', (req, res) => {
   const { uid } = req.query;
   const uid_arr = uid.split(',').map(s => +s.trim());
@@ -49,13 +53,64 @@ app.post('/data', (req, res) => {
   
   return send(res, 200, d, { 'Content-Type': 'application/json' });
 });
+
 app.get('/', async (_, res) => {
   const msg = 'Hello, World!';
   const d = await html`<h1>${msg}</h1>`;
   
   return send(res, 200, d, { 'Content-Type': 'text/html' });
 });
-app.post('/form', 
+
+app.get('/form', async (req, res) => {
+  const csrf = uuid();
+  const html_form = await html`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <title>Form</title>
+      </head>
+      <body>
+        <div id="user-submission">
+          <form action="form-submission" method="post" enctype="application/x-www-form-urlencoded" >
+            <fieldset>
+              <label for="firstName">First Name:</label>
+              <input type="text" name="firstName" />
+
+              <label for="lastName">Last Name:</label>
+              <input type="text" name="lastName" />
+
+              <label for="dob">Date of Birth:</label>
+              <input type="text" name="dob" />
+
+              <label for="email">email:</label>
+              <input type="text" name="email" />
+
+              <label for="gender">Gender:</label>
+              <select name="gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+
+              <input type="hidden" id="_csrf" name="_csrf" value="${csrf}" />
+
+              <input type="submit" value="Submit" />
+            </fieldset>
+          </form>
+        </div>
+      </body>
+    </html>
+  `;
+
+  send(res, 200, html_form, { 'Content-Type': 'text/html' });
+
+});
+
+app.post('/form-submission', (req, res) => {
+  const { firstName, lastName, dob, email, gender, _csrf } = req.query;
+
+  res.end("txt form submitted successfully");
+});
 
 app
   .listen(PORT)
